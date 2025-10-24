fd8c66517991042dc96e8a3a86084f185fa1e982
SAEMMM <saem030@naver.com>
2025-10-24 11:30:36 +0900

    chore: tailwindcss, shadcn 설치

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,22 @@
 import "./App.css";
+import { Button } from "./components/ui/button";
+import { cn } from "./lib/utils";
 
 function App() {
-  return <div>hello World</div>;
+  const isActive = false;
+
+  return (
+    <div>
+      <Button>버튼</Button>
+
+      <div className={cn(isActive ? "text-green-500" : "text-red-500")}>
+        isActive
+      </div>
+      <div className="text-primary">Primary</div>
+      <div className="text-muted">Muted</div>
+      <div className="text-destructive">Destructive</div>
+    </div>
+  );
 }
 
 export default App;
